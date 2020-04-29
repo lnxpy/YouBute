ba3b15d846a14467ae71d93608b9dfe347528658
lnxpy <lnxpylnxpy@gmail.com>
2020-04-29 11:53:35 +0430

    security commit

--- youbute/settings.py
+++ youbute/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hfo9deex5ke0q7x^61l0%%a%c$reeg$tbf_!v#^$$9kp1-_6g1'
+th open(os.path.join(BASE_DIR, 'youbute/secret.txt')) as sec:
+        SECRET_KEY = sec.read().strip()))''))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
